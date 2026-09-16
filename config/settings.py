ba650c6af5c4d5f939d1e465ed8813d647c24906
Chen-wunyi <chen.wunyi@gmail.com>
2026-09-16 15:12:05 +0800

    Update code changes and documentation

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2%ba9&50g1bgu)s&quz)0u-6-)p)+75+l#5yo7&4&sfca0pc25'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
